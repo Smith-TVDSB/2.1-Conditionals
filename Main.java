32c74dcf211cd8a60beb359e44abc10f657dc9e8
Keith Smith <keith.smith@tvdsb.ca>
2020-08-31 20:24:45 +0000

    Updated to 2.2

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    String lastName="Targaryen";
+    String lastName="Stark";
     System.out.println ("Do you have a claim to the iron throne?");
-    if (lastName=="Targaryen"){ // Since it's true it will run the code
-      System.out.println("Yes you do Targaryen!"); // This will run
+    if (lastName=="Targaryen"){
+      System.out.println("Yes you do Targaryen!"); 
     }
+    else if (lastName=="Lannister") {
+      System.out.println("Yes you do Lannister");
+    }
+    else if (lastName=="Baratheon"){
+      System.out.println("Yes you do Baratheon");
+    }
+    else {
+      System.out.println("Sorry you have no claim to the iron throne.");
+    }// It checks each case and if none are true it does the else statement
   }
 }

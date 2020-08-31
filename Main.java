e27cb8babd56800c96fe471e651186fd3e1b0367
Keith Smith <keith.smith@tvdsb.ca>
2020-08-31 18:23:57 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    String lastName="Targaryen";
+    System.out.println ("Do you have a claim to the iron throne?");
+    if (lastName=="Targaryen"){ // Since it's true it will run the code
+      System.out.println("Yes you do Targaryen!"); // This will run
+    }
+  }
+}
---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# 2.1A Modify the iron throne program
+
+### Modify the program so that:
+1. The user can enter in a first name
+2. The user can enter the last name
+3. If the last name is Baratheon then they have a claim to the throne
+4. Don't forget to fix the output screen (don't say targareon) 
